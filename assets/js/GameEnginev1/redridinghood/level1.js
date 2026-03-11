cd26ae3f867100363cedd1f04d6591d8620dae18
Rashig-1804 <rashig1804@gmail.com>
2026-03-11 15:12:56 -0700

    added correct rows and columns

--- assets/js/GameEnginev1/redridinghood/level1.js
+++ assets/js/GameEnginev1/redridinghood/level1.js
@@ -23,8 +23,8 @@ class GameLevelRedRidingHood1 {
       pixels: { height: 192, width: 144 },
       orientation: { rows: 4, columns: 3 },
       down: { row: 0, start: 0, columns: 3 },
-      left: { row: 2, start: 0, columns: 3 },
-      right: { row: 1, start: 0, columns: 3 },
+      left: { row: 1, start: 0, columns: 3 },
+      right: { row: 2, start: 0, columns: 3 },
       up: { row: 3, start: 0, columns: 3 },
       keypress: { up: 87, left: 65, down: 83, right: 68 }
     };
