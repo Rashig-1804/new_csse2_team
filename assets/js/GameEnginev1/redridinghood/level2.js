a20d41334cc8cbe8d8f2cafd0ce03730dfe877a9
Anika2317 <anika.seksaria@gmail.com>
2026-03-12 15:21:01 -0700

    added wolf to level 2

--- assets/js/GameEnginev1/redridinghood/level2.js
+++ assets/js/GameEnginev1/redridinghood/level2.js
@@ -1,6 +1,37 @@
 // level2.js - Red Riding Hood Level 2: The Chase
 import GameEnvBackground from '../essentials/GameEnvBackground.js';
 import Player from '../essentials/Player.js';
+import Character from '../essentials/Character.js';
+
+class Wolf extends Character {
+  constructor(data, gameEnv) {
+    super(data, gameEnv);
+
+    this.position = data.INIT_POSITION || { x: -this.width, y: gameEnv.innerHeight * 0.5 };
+    this.velocity = { x: data.SPEED || 2, y: 0 };
+    this.wrap = data.WRAP || true;
+  }
+
+  update() {
+    this.draw();
+    this.collisionChecks();
+    this.move();
+
+    // Loop the wolf back to left side once it leaves right side
+    if (this.position.x > this.gameEnv.innerWidth) {
+      this.position.x = -this.width;
+    }
+    if (this.position.x + this.width < 0) {
+      this.position.x = this.gameEnv.innerWidth;
+    }
+  }
+
+  // Override to let the wolf move continuously rather than being stopped by edges
+  move() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
 
 class GameLevelRedRidingHood2 {
   constructor(gameEnv, game) {
@@ -49,9 +80,24 @@ class GameLevelRedRidingHood2 {
       keypress: { up: 87, left: 65, down: 83, right: 68 }
     };
 
+    const sprite_data_wolf = {
+      id: 'Wolf',
+      src: path + "/images/gamify/ridinghood/wolfff.png",
+      SCALE_FACTOR: 3.5, // Lowered scale because 632px is already very large
+      STEP_FACTOR: 1000,
+      ANIMATION_RATE: 8,
+      INIT_POSITION: { x: 400, y: height - (height / 3.0) }, 
+      pixels: { height: 395, width: 632 }, // UPDATED TO MATCH YOUR LOGS
+      orientation: { rows: 1, columns: 1 },
+      direction: 'right',
+      SPEED: 2,
+      zIndex: 20
+    };
+
     const list = [
       { class: GameEnvBackground, data: image_data_chase },
-      { class: Player, data: sprite_data_red }
+      { class: Player, data: sprite_data_red },
+      { class: Wolf, data: sprite_data_wolf }
     ];
 
     this.classes = list;
@@ -61,25 +107,30 @@ class GameLevelRedRidingHood2 {
 
     this.background = new GameEnvBackground(image_data_chase, gameEnv);
     this.player = new Player(sprite_data_red, gameEnv);
+    this.wolf = new Wolf(sprite_data_wolf, gameEnv);
   }
 
   update() {
     if (this.player) this.player.update();
+    if (this.wolf) this.wolf.update();
   }
 
   draw() {
     if (this.background) this.background.draw();
     if (this.player) this.player.draw();
+    if (this.wolf) this.wolf.draw();
   }
 
   resize() {
     if (this.background) this.background.resize();
     if (this.player) this.player.resize();
+    if (this.wolf) this.wolf.resize();
   }
 
   destroy() {
     if (this.background) this.background.destroy();
     if (this.player) this.player.destroy();
+    if (this.wolf) this.wolf.destroy();
     if (this.titleElement && this.titleElement.parentNode) this.titleElement.remove();
   }
 }
